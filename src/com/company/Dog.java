d763b390dcaa3ec47f171209949c0270e21fe6b5
Adilthebest <adilthebestw27@gmail.com>
2022-03-09 20:46:31 +0600

    Initial commit

---
+++ src/com/company/Dog.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Dog extends Animal {
+    private String Damage;
+
+
+    public Dog(int age, String name, String damage) {
+        super(age, name);
+        this.Damage = damage;
+    }
+
+    public String getDamage() {
+        return Damage;
+    }
+
+    @Override
+    public void print() {
+        System.out.println(getInfo() + "\nDamage: " + Damage);
+    }
+}
